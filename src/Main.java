3baf641577298ecf8a4a56483eb0082bcbb8a7e7
LivNTI <107177980+LivNTI@users.noreply.github.com>
2023-08-29 14:47:44 +0200

    Fixed: printign guessed word printing lists reducing lives

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
 
         // choose a word
@@ -30,6 +29,19 @@ public class Main {
 
         //separate the string into a list of the letters
         String[] letters = word.split("");
+        printList(letters);
+
+        // and creat a list for the guessed word
+        String[] guessedWord = new String[letters.length];
+        int count = 0; // this is a counter
+        while (count < guessedWord.length) {
+            guessedWord[count] = "_";
+            count++;
+        }
+        printList(guessedWord);
+
+        // how many lives (tries) should our user have
+        int lives = 3;
 
         //user input
         Scanner myScan = new Scanner(System.in);
@@ -39,25 +51,35 @@ public class Main {
         //System.out.println("user guess is " + guess);
 
         //chekc if guess is in the word
+        boolean isInWord = false;
         for (int i = 0; i < letters.length; i++) {
+            // if guess is correct print the letter
             if (guess.equals(letters[i])) {
-                System.out.println("letter exist on index " + i);
+                //System.out.println("letter exist on index " + i);
+                // guessedWord[i] = letters[i]; //slightly less optimal
+                guessedWord[i] = guess;
+                isInWord = true;
             }
+        }
+        if (!isInWord) { // same as isInWord == false
+            // if isInWord == true --> !isInWord == false
+            // if isInWord == false --> !isInWord == true
+            lives--;
 
         }
+        printList(guessedWord);
+        System.out.println(" you now have " + lives + " lives left");
+
+    }
 
 
-
-
-        /*
+    public static void printList(String[] myList) {
         // Check the contents of a list
-        for (int i = 0; i < letters.length; i++) {
-            System.out.print(letters[i]);
+        for (int i = 0; i < myList.length; i++) {
+            System.out.print(myList[i]);
 
         }
-         */
-
-
+        System.out.println();//prints a linebreak
     }
 }
 
