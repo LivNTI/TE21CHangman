820894c7da83720730b21f0d066badd547190f64
LivNTI <107177980+LivNTI@users.noreply.github.com>
2023-09-04 10:44:53 +0200

    Code cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,7 @@
 /*
-  TODO: what i need to do in my hangman
-   * Ha ett ord
-     - Statiskt?
-     - Random från lista?
-   * Dela upp ordet
-   * Input
-     - Kolla att det bokstav
-   * Finns bokstaven i ordet?
-   * Var finns bokstaven
-   * SKriv ut bokstaven
-   * Ta bort ett liv
-   * Hur många liv?
-   * Fortsätt tills vi är “klara”
-     - Hela ordet gissat
-     - Inga liv
-
+ * This is TE21Cs hangman
+ *
+ * @AUTHOR LivNTI
  */
 
 import java.util.Scanner;
@@ -24,7 +11,6 @@ public class Main {
         System.out.println("Hello world!");
 
         // choose a word
-        // char aCharacter = 'g'; // this a character
         String word = "teknik";
 
         // Declaring variables for later use
@@ -59,8 +45,6 @@ public class Main {
             } while (!guess.matches("[a-öA-Ö]+")
                     || guess.length() != 1);
 
-            //System.out.println("user guess is " + guess);
-
             wordCompleted = true;
 
             //chekc if guess is in the word
@@ -68,8 +52,6 @@ public class Main {
             for (int i = 0; i < letters.length; i++) {
                 // if guess is correct print the letter
                 if (guess.equalsIgnoreCase(letters[i])) {
-                    //System.out.println("letter exist on index " + i);
-                    // guessedWord[i] = letters[i]; //slightly less optimal
                     guessedWord[i] = guess.toUpperCase();
                     isInWord = true;
                 }
@@ -77,27 +59,22 @@ public class Main {
                     wordCompleted = false;
                 }
             }
-            if (!isInWord) { // same as isInWord == false
-                // if isInWord == true --> !isInWord == false
-                // if isInWord == false --> !isInWord == true
+            // reduces lives if letter is not in word
+            if (!isInWord) {
                 lives--;
-
             }
             printList(guessedWord);
             System.out.println(" you now have " + lives + " lives left");
-            // System.out.println("word COmp is " + wordCompleted);
         } while ((lives > 0) && (wordCompleted == false));
-
     }
 
-
+    // prints a list
     public static void printList(String[] myList) {
         // Check the contents of a list
         for (int i = 0; i < myList.length; i++) {
             System.out.print(myList[i]);
-
         }
-        System.out.println();//prints a linebreak
+        System.out.println(); //prints a linebreak
     }
 }
 
